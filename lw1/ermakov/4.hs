17586eea41d9e496ef567e119784f40186681eac
Pavel Ermakov <pavel.ermakov.acc@outlook.com>
2024-01-22 23:22:20 +0300

    lw1: Упрощено 4 задание

--- lw1/ermakov/4.hs
+++ lw1/ermakov/4.hs
@@ -1,10 +1,6 @@
-triangleNumbers :: Int -> Int -> [Int]
-triangleNumbers start count =
-    [n * (n + 1) `div` 2 | n <- [start..start + count - 1]]
-
-triangleNumbersStartedWithFirst :: Int -> [Int]
-triangleNumbersStartedWithFirst count = triangleNumbers 1 count
+triangleNumbers :: Int -> [Int]
+triangleNumbers count = [n * (n + 1) `div` 2 | n <- [1..count]]
 
 main :: IO ()
 main = do
-    print $ triangleNumbersStartedWithFirst 50
+    print $ triangleNumbers 50
