25bf177717db7f3c448689dfae4e1ed7795c565b
AndreyM <222mushkin@gmail.com>
2024-01-24 22:30:16 +0300

    mushkin Убрал/добавил пустые строки

--- lw2/mushkin/4.hs
+++ lw2/mushkin/4.hs
@@ -5,7 +5,6 @@ position list atom
     | not (elem atom list) = -1 -- item is not in the list
     | otherwise = until (\i -> list !! i == atom) (+1) 0
 
-
 main = do
     print (position [1, 2, 3, 4, 5] 4)
     print (position [1, 2, 3, 4, 5] 9)
