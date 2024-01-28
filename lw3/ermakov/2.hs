9768ab6c380cd64551910f0f53ef1751c96dbc6d
Pavel Ermakov <pavel.ermakov.acc@outlook.com>
2024-01-28 20:45:26 +0300

    lw3: Поправлены замечания PR'а

--- lw3/ermakov/2.hs
+++ lw3/ermakov/2.hs
@@ -3,6 +3,6 @@ main :: IO()
 secondlastlist :: [[a]] -> [a]
 secondlastlist (x:xs)
     | null x = []
-    | otherwise = [last x] ++ secondlastlist xs
+    | otherwise = last x : secondlastlist xs
 
 main = print $ secondlastlist [[1,2,3],[]]
