eef9f73f00791ed1ed7823a610bc516e97efac98
AndreyM <222mushkin@gmail.com>
2024-01-28 19:29:33 +0300

    mushkin Правки по CR

--- lw3/mushkin/3.hs
+++ lw3/mushkin/3.hs
@@ -1,10 +1,7 @@
-accumulate :: Eq a => [a] -> [a] -> [a]
-accumulate [] xs = xs
-accumulate xs [] = xs
-accumulate acc (x:xs) = (if not (elem x acc) then [x] else []) ++ accumulate acc xs
-
 myunion :: Eq a => [a] -> [a] -> [a]
-myunion xs ys = accumulate (accumulate [] xs) ys
+myunion [] xs = xs
+myunion xs [] = xs
+myunion acc (x:xs) = (if not (elem x acc) then [x] else []) ++ myunion acc xs
 
 main :: IO()
 main = print (myunion [1,2,3,4] [3,2,1,0,5,6])
--- lw3/mushkin/4.hs
+++ lw3/mushkin/4.hs
@@ -1,7 +1,9 @@
 mysubst :: Eq a => [a] -> [a] -> [a]
 mysubst [] _  = []
 mysubst xs [] = xs
-mysubst (x:xs) ys = if elem x ys then mysubst xs ys else x:(mysubst xs ys)
+mysubst (x:xs) ys
+    | elem x ys = mysubst xs ys
+    | otherwise = x : (mysubst xs ys)
 
 main :: IO()
 main = print $ mysubst [1, 2, 3, 4, 5] [1, 2, 3]
