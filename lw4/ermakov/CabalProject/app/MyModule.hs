3009c89ba8833b1085ffff38b92e75189199d393
Pavel Ermakov <pavel.ermakov.acc@outlook.com>
2024-02-07 20:54:24 +0300

    lw4: Добавлена функция из модуля Map и функция из модуля Char

--- lw4/ermakov/CabalProject/app/MyModule.hs
+++ lw4/ermakov/CabalProject/app/MyModule.hs
@@ -14,7 +14,7 @@ myIntersperse sep (x:xs) = x : sep : myIntersperse sep xs
 myTranspose :: [[a]] -> [[a]]
 myTranspose [] = []
 myTranspose ([] : xss) = myTranspose xss
-myTranspose ((x:xs) : xss) = (x : [h | (h:_) <- xss]) : myTranspose (xs : [ t | (_:t) <- xss])
+myTranspose ((x:xs) : xss) = (x : [h | (h:_) <- xss]) : myTranspose (xs : [t | (_:t) <- xss])
 
 -- Реализация fromList для Set
 myFromList :: (Ord a) => [a] -> Set.Set a
@@ -27,3 +27,21 @@ myPartition p xs = foldr (\x (yes, no) -> if p x then (x:yes, no) else (yes, x:n
 -- Реализация intercalate
 myIntercalate :: [a] -> [[a]] -> [a]
 myIntercalate xs xss = concat (myIntersperse xs xss)
+
+-- Функция, проверяющая, является ли символ шестнадцатеричной цифрой
+myIsHexDigit :: Char -> Bool
+myIsHexDigit c = (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')
+
+data Map k v = Empty
+             | Node k v (Map k v) (Map k v)
+             deriving (Show)
+
+-- Проверка наличия ключа в Map
+myMember :: (Ord k) => k -> Map k v -> Bool
+myMember _ Empty = False
+myMember key (Node k v left right)
+  | key == k = True
+  | key < k  = myMember key left
+  | key > k  = myMember key right
+
+
