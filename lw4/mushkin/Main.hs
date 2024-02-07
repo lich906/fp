b487368f252a24ab46eb83df8af98da455692f36
AndreyM <222mushkin@gmail.com>
2024-02-07 22:32:00 +0300

    master mushkin lw4: Добавил примеров использования

--- lw4/mushkin/Main.hs
+++ lw4/mushkin/Main.hs
@@ -5,10 +5,14 @@ import qualified Data.Set as Set
 main :: IO()
 main = do
     print $ MyModule.zip [1, 2, 3, 4, 5] [6, 7, 8] -- [(1,6),(2,7),(3,8)]
+    print $ MyModule.zip [4, 5] [6, 7, 8] -- [(4,6),(5,7)]
 
     print $ MyModule.delete 't' "hi out there!" -- "hi ou there!"
+    print $ MyModule.delete 'h' "hi out there!" -- "i out there!"
 
     print $ MyModule.break (==4) [1, 2, 3, 4, 5, 6, 7] -- ([1,2,3],[4,5,6,7])
+    print $ MyModule.break (==8) [1, 2, 3, 4, 5, 6, 7] -- ([1,2,3,4,5,6,7],[])
+    print $ MyModule.break (==1) [1, 2, 3, 4, 5, 6, 7] -- ([],[1,2,3,4,5,6,7])
 
     print $ MyModule.member 3 (Map.fromList [(2, 'a'), (3, 'b'), (7, 'e')]) -- True
     print $ MyModule.member 3 (Map.fromList [(2, 'a'), (4, 'b'), (7, 'e')]) -- False
