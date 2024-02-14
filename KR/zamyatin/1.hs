79cdbe98f69dcaff43ea8791fbb0cae59c59ea13
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2024-02-14 19:32:36 +0300

    +

--- KR/zamyatin/1.hs
+++ KR/zamyatin/1.hs
@@ -13,11 +13,19 @@ partitionNImpl :: [[a]] -> Int-> [[a]]
 partitionNImpl xxs n    | length (last xxs) <= n = xxs
                         | otherwise = partitionNImpl (take (length xxs - 1) xxs ++ [take n (last xxs)]++ [lastN' (length (last xxs) - n) (last xxs)]) n
 
+--расчёт длины итогового подлиста (кроме последнего)
+getSubListLenght::Int->Int->Int->Int
+getSubListLenght l value n  | l - value * (n - 1) <= 0 = error "impossible to divide"
+                        | l - value * (n - 1) > value = getSubListLenght l (value + 1) n
+                        | otherwise = value
+
 -- n-1 одинаковых подсписков, последний список меньше остальных
 partitionN :: [a] -> Int -> [[a]]
 partitionN  [] _ = []
-partitionN xs n | length xs `mod` n == 0 = partitionNImpl [xs] (length xs `div` n )
-                | otherwise = partitionNImpl [xs] ((length xs `div` n ) + 1)
+partitionN xs n = partitionNImpl [xs] (getSubListLenght (length xs) 1 n)
 
 main :: IO()
-main = print (partitionN [1,2,3,4,5] 2)
+main = do
+    print (partitionN [1,2,3,4,5] 2)
+    print (partitionN [1,2,3,4,5] 3)
+    print (partitionN [1,2,3,4,5,6,7, 8] 3)
