13bb9b96881fd7da35777fdabc6df4429a9f7411
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2024-02-14 14:22:49 +0300

    перенёс файлы в свою папку

--- KR/zamyatin/1.hs
+++ KR/zamyatin/1.hs
@@ -9,12 +9,12 @@ lastN' :: Int -> [a] -> [a]
 lastN' n xs = foldl' takeLeftover xs (drop n xs)
 
 --взять последний список входного списка, выделить из него N значений в отдельный список
-partitionNImpl :: [[a]] -> Int->[[a]]
+partitionNImpl :: [[a]] -> Int-> [[a]]
 partitionNImpl xxs n    | length (last xxs) <= n = xxs
                         | otherwise = partitionNImpl (take (length xxs - 1) xxs ++ [take n (last xxs)]++ [lastN' (length (last xxs) - n) (last xxs)]) n
 
 -- n-1 одинаковых подсписков, последний список меньше остальных
-partitionN :: [a] -> Int ->[[a]]
+partitionN :: [a] -> Int -> [[a]]
 partitionN  [] _ = []
 partitionN xs n | length xs `mod` n == 0 = partitionNImpl [xs] (length xs `div` n )
                 | otherwise = partitionNImpl [xs] ((length xs `div` n ) + 1)
--- KR/zamyatin/2.hs
+++ KR/zamyatin/2.hs
@@ -2,4 +2,4 @@ elemIndices :: Eq a => a -> [a] -> [Int]
 elemIndices x xs = fst (foldr (\t (result, i) -> if t == x then (result ++ [i], i + 1) else (result, i + 1)  ) ([], 0) xs)
 
 main :: IO()
-main = print (elemIndices 1 [1,2,3,1,3,4,1])
+main = print (elemIndices 1 [1, 2, 3, 1, 3, 4, 1])
