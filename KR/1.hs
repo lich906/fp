ad16dd891b7199f3a011d35bc5486158f9800135
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2024-02-14 09:17:27 +0300

    1 задание

---
+++ KR/1.hs
@@ -0,0 +1,23 @@
+import Data.Foldable
+
+takeLeftover :: [a] -> t -> [a]
+takeLeftover [] _ = []
+takeLeftover (x:xss) _ = xss
+
+lastN' :: Int -> [a] -> [a]
+lastN' n xs = foldl' takeLeftover xs (drop n xs)
+
+--взять последний список входного списка, выделить из него N значений в отдельный список
+partitionNImpl :: [[a]] -> Int->[[a]]
+partitionNImpl xxs n    | length (last xxs) <= n = xxs
+                        | otherwise = partitionNImpl (take (length xxs - 1) xxs ++ [take n (last xxs)]++ [lastN' (length (last xxs) - n) (last xxs)]) n
+
+-- n-1 одинаковых подсписков, последний список меньше остальных
+partitionN :: [a] -> Int ->[[a]]
+partitionN  [] _ = []
+partitionN xs n | length xs `mod` n == 0 = partitionNImpl [xs] (length xs `div` n )
+                | otherwise = partitionNImpl [xs] ((length xs `div` n ) + 1)
+
+
+main :: IO()
+main = print (partitionN [1,2,3,4,5] 2)
